47097ede32d7a170d420da0900590781117653f3
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-10-10 14:53:14 -0500

    Adds code for updating profile media

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,10 +1,26 @@
-import { setRegisterFormListener } from "./handlers/register.mjs";
-import { setLoginFormListener } from "./handlers/login.mjs";
+import * as listeners from "./handlers/index.mjs";
+import * as templates from "./templates/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
-if(path === '/profile/login/index.html') {
-  setLoginFormListener();
+console.log(path)
+
+if (path === '/profile/login/index.html') {
+  listeners.setLoginFormListener()
 } else if (path === '/profile/register/index.html') {
-  setRegisterFormListener();
+  listeners.setRegisterFormListener()
+} else if (path === '/post/create/index.html') {
+  listeners.setCreatePostFormListener()
+} else if (path === '/post/edit/index.html') {
+  listeners.setUpdatePostListener()
+} else if (path === '/profile/edit/index.html') { listeners.setUpdateProfileListener()
 }
+
+// async function testTemplate() {
+//   const posts = await postMethods.getPosts();
+//   const container = document.querySelector("#posts");
+//   templates.renderPostTemplates(posts, container);
+// }
+
+// testTemplate()
