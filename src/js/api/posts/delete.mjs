1b50ae1f94611629bfd89b4a26d6e4dc93d08cbd
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-10-14 11:52:58 -0500

    delete post

--- src/js/api/posts/delete.mjs
+++ src/js/api/posts/delete.mjs
@@ -5,16 +5,20 @@ import { authFetch } from "../authFetch.mjs";
 const action = "/posts";
 const method = "delete";
 
+const bin = document.querySelector(".trash-can")
+
 export async function removePost(id) {
   if(!id) {
     throw new Error ("Delete requires a postID");
   }
 
-  const updatePostURL = `${API_SOCIAL_URL}${action}/${id}`;
+  const deletePostURL = `${API_SOCIAL_URL}${action}/${id}`;
 
-  const response = await authFetch(updatePostURL, {
+  const response = await authFetch(deletePostURL, {
     method,
   })
 
   return await response.json()
 } 
+
+bin.addEventListener("onclick", removePost(id));
