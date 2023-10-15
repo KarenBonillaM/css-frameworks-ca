c7db06237831467d5316a7e9a8a364f86736063a
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-10-14 22:07:11 -0500

    templates posts

---
+++ src/js/templates/posts.mjs
@@ -0,0 +1,22 @@
+import * as templates from "./index.mjs";
+import * as postMethods from "../api/posts/index.mjs";
+
+async function testTemplate() {
+  const posts = await postMethods.getPosts();
+  const container = document.querySelector("#posts");
+  templates.renderPostTemplates(posts, container);
+}
+
+testTemplate()
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+async function testPostTemplate() {
+  const post = await postMethods.getPost(id);
+  const container = document.querySelector("#post");
+  templates.renderPostTemplate(post, container);
+}
+
+testPostTemplate()
