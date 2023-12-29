f4d5db9e49c51d205dad5d5de908cc398debfe9e
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-12-29 23:11:35 +0100

    fetch post user

--- src/js/api/profile/postsUser.mjs
+++ src/js/api/profile/postsUser.mjs
@@ -1,26 +1,20 @@
 import { API_SOCIAL_URL } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
 
-const action = "/profiles";
-const userName = "/Margarita";
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const name = params.get("name");
+
+console.log(name);
+
+
+const action = "/profiles/";
 const userPostURL = "/posts";
 
 export async function getPostsUser() {
-  const getPostsUserURL = `${API_SOCIAL_URL}${action}${userName}${userPostURL}`;
+  const getPostsUserURL = `${API_SOCIAL_URL}${action}${name}${userPostURL}`;
 
   const response = await authFetch(getPostsUserURL)
 
   return await response.json()
 } 
-
-// export async function getPostUser(id) {
-//   // if(!id) {
-//   //   throw new Error ("Get requires a postID");
-//   // }
-
-//   const getPostURL = `${API_SOCIAL_URL}${action}/${id}`;
-
-//   const response = await authFetch(getPostURL)
-
-//   return await response.json()
-// } 
