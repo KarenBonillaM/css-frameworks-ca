46b5505646b4778a65ecbe8e3311422dade28422
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-12-29 12:51:56 +0100

    Implement search functionality

--- src/js/handlers/search.mjs
+++ src/js/handlers/search.mjs
@@ -1,54 +1,22 @@
-// const searchContainer = document.querySelector("#search-container");
 const search = document.querySelector("#search");
-// const searchButton = document.querySelector("#search-button");
 const postsContainer = document.querySelector(".postsContainer")
 
-  search.onkeyup = function() {
-    const searchBox = document.querySelector("#search").value.toLowerCase();
-      const post = document.querySelectorAll(".post")
-      const postTitle = postsContainer.getElementsByTagName("h2");
+search.onkeyup = function() {
+  const searchBox = document.querySelector("#search").value.toLowerCase();
+  const post = document.querySelectorAll(".post")
+  const postTitle = postsContainer.getElementsByTagName("h2");
+      
+  for(let i = 0; i < postTitle.length; i++) {
+  let match = post[i].getElementsByTagName("h2")[0];
     
+  if(match) {
+    let textValue = match.textContent || match.innerHTML
     
-    
-      for(let i = 0; i < postTitle.length; i++) {
-        let match = post[i].getElementsByTagName("h2")[0];
-    
-        if(match) {
-          let textValue = match.textContent || match.innerHTML
-    
-          if(textValue.toLowerCase().indexOf(searchBox) > -1) {
-            post[i].style.display = "";
-          } else {
-            post[i].style.display = "none";
-          }
-        }
-      }
+    if(textValue.toLowerCase().indexOf(searchBox) > -1) {
+    post[i].style.display = "";
+    } else {
+    post[i].style.display = "none";
     }
-
-
-const searchPostsUser = document.querySelector("#searchPostsUser");
-// const searchButton = document.querySelector("#search-button");
-const postsContainerPostsUser = document.querySelector(".postsUserContainer")
-
-  searchPostsUser.onkeyup = function() {
-    const searchBox = document.querySelector("#searchPostsUser").value.toLowerCase();
-      const post = document.querySelectorAll(".post")
-      const postTitle = postsContainerPostsUser.getElementsByTagName("h2");
-    
-    
-    
-      for(let i = 0; i < postTitle.length; i++) {
-        let match = post[i].getElementsByTagName("h2")[0];
-    
-        if(match) {
-          let textValue = match.textContent || match.innerHTML
-    
-          if(textValue.toLowerCase().indexOf(searchBox) > -1) {
-            post[i].style.display = "";
-          } else {
-            post[i].style.display = "none";
-          }
-        }
-      }
-    }
-
+  }
+ }
+}
