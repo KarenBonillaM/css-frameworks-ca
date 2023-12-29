4f096e33f196db916eae6cc352a4dfbbeb90a666
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-12-29 18:31:53 +0100

    Display post details

--- src/js/templates/userPostEdit.mjs
+++ src/js/templates/userPostEdit.mjs
@@ -1,94 +1,75 @@
-// export function postTemplateD(postData) {
-//   const post = document.createElement("a");
-//   post.setAttribute("href", `/post/edit/index.html?id=${postData.id}`)
-//   // post.setAttribute("post-id", postData.id);
-//   post.classList.add("post");
-//   post.classList.add("row");
-//   post.classList.add("my-5");
-//   post.classList.add("py-3");
-//   post.classList.add("card");
-//   post.classList.add("bg-secondary");
-//   post.classList.add("align-items-center");
-
-//   const postTitle = document.createElement("h2");
-//   postTitle.classList.add("post-title");
-//   postTitle.innerText = postData.title;
-  
-//   post.append(postTitle)
-
-//   if(postData.media) {
-//     const img = document.createElement('img');
-//     img.classList.add("postImg");
-//     img.classList.add("img-thumbnail");
-//     img.classList.add("border-0");
-//     img.classList.add("bg-secondary");
-//     img.src = postData.media;
-//     img.alt = `Image from ${postData.title}`;
-//     post.append(img)
-//   }
-
-//   if(!postData.media === true) {
-//     post.style.display = "none";
-//   }
-
-//   if(postData.body) {
-//     const body = document.createElement("p");
-//     body.classList.add("mt-4")
-//     body.innerText = postData.body;
-//     post.append(body)
-//   }
-
-//   const updatePostContainer = document.createElement("div");
-//   updatePostContainer.classList.add("updatePostContainer");
-//   updatePostContainer.classList.add("row")
-//   updatePostContainer.classList.add("align-items-center")
-//   updatePostContainer.classList.add("pt-2")
-
-//   post.append(updatePostContainer);  
-
-//   const updateButtonLink = document.createElement("a");
-//   updateButtonLink.setAttribute("href", `/post/edit/index.html?id=${postData.id}`);
-//   updateButtonLink.classList.add("col")
-
-//   updatePostContainer.append(updateButtonLink)
-
-//   const updateButton = document.createElement("button");
-//   updateButton.classList.add("btn");
-//   updateButton.classList.add("btn-primary");
-//   updateButton.innerText = "Update Post";
-
-//   updateButtonLink.append(updateButton);
-
-//   const trashCan = document.createElement("i");
-//   trashCan.classList.add("fa-solid");
-//   trashCan.classList.add("fa-trash-can");
-//   trashCan.classList.add("col");
-//   trashCan.classList.add("deletePostBin");
-
-
-//   updatePostContainer.append(trashCan)
-
-  
-//   return post;
-// }
-
 export function postTemplateE(postData) {
+  const titleDetailsPage = document.querySelector(".post-details-page-title");
+  titleDetailsPage.innerText = `${postData.title}`;
+
+  const userIconLink = document.querySelector(".user-icon-link");
+  userIconLink.href = `/profile/index.html?name=${postData.author.name}`
+
   const post = document.createElement("div");
-  post.setAttribute("href", `/post/edit/index.html?id=${postData.id}`)
-  // post.setAttribute("post-id", postData.id);
+  // post.setAttribute("href", `/post/edit/index.html?id=${postData.id}`)
   post.classList.add("post");
   post.classList.add("row");
   post.classList.add("my-5");
-  post.classList.add("py-3");
+  post.classList.add("p-3");
   post.classList.add("card");
   post.classList.add("bg-secondary");
   post.classList.add("align-items-center");
+
+  const postHeader = document.createElement("div");
+  postHeader.classList.add("post-header");
+  postHeader.classList.add("d-flex");
+  postHeader.classList.add("align-items-center");
+  postHeader.classList.add("py-3");
+
+  post.append(postHeader);
+
+
+  const postAuthorInfoContainer = document.createElement("div");
+  postAuthorInfoContainer.classList.add("d-flex");
+  postAuthorInfoContainer.classList.add("col-4");
+  postAuthorInfoContainer.classList.add("align-items-center");
+
+  postHeader.append(postAuthorInfoContainer);
+
+  if(postData.author && postData.author.name) {
+
+    const postAuthorName = postData.author.name;
+
+    const authorImgContainer = document.createElement("span");
+    authorImgContainer.classList.add("userimage");
+
+    postAuthorInfoContainer.append(authorImgContainer);
+
+    const postAuthorAvatar = postData.author.avatar;
+
+    const authorAvatar = document.createElement("img");
+    authorAvatar.classList.add("img-fluid");
+    authorAvatar.classList.add("post-img");
+    authorAvatar.alt = postAuthorName;
+    authorAvatar.src = postAuthorAvatar;
+
+    if(!postData.author.avatar) {
+      authorAvatar.src = "/images/profile-picture.jpg"
+    }
+
+    authorImgContainer.append(authorAvatar);
+
+    const authorNameContainer = document.createElement("span");
+    authorNameContainer.classList.add("username");
+    authorNameContainer.classList.add("fs-6");
+    authorNameContainer.classList.add("ps-2");
+    authorNameContainer.classList.add("pe-2");
+    authorNameContainer.classList.add("fw-medium");
+    authorNameContainer.innerText = postAuthorName;
+
+    postAuthorInfoContainer.append(authorNameContainer);
+  }
 
   const postTitle = document.createElement("h2");
   postTitle.classList.add("post-title");
   postTitle.innerText = postData.title;
   
-  post.append(postTitle)
+  postHeader.append(postTitle)
 
   if(postData.media) {
     const img = document.createElement('img');
@@ -112,19 +93,37 @@ export function postTemplateE(postData) {
     post.append(body)
   }
 
-  const updatePostContainer = document.createElement("div");
-  updatePostContainer.classList.add("updatePostContainer");
-  updatePostContainer.classList.add("row")
-  updatePostContainer.classList.add("align-items-center")
-  updatePostContainer.classList.add("pt-2")
+  // const updatePostContainer = document.createElement("div");
+  // updatePostContainer.classList.add("updatePostContainer");
+  // updatePostContainer.classList.add("row")
+  // updatePostContainer.classList.add("align-items-center")
+  // updatePostContainer.classList.add("pt-2")
 
-  post.append(updatePostContainer);  
+  // post.append(updatePostContainer); 
 
-  const updateButtonLink = document.createElement("a");
-  updateButtonLink.setAttribute("href", `/post/edit/index.html?id=${postData.id}`);
-  updateButtonLink.classList.add("col")
+  const detailsPostContainer = document.createElement("div");
+  detailsPostContainer.classList.add("post-details-container");
+  detailsPostContainer.classList.add("d-flex");
+  detailsPostContainer.classList.add("justify-content-around");
 
-  updatePostContainer.append(updateButtonLink)
+  post.append(detailsPostContainer);
+
+  const commentsCount = document.createElement("div");
+  commentsCount.innerText =`Comments ${postData._count.comments}`;
+
+  detailsPostContainer.append(commentsCount);
+
+  const reactionsCount = document.createElement("div");
+  reactionsCount.innerText =`Reactions ${postData._count.reactions}`;
+
+  detailsPostContainer.append(reactionsCount)
+  
+
+  // const updateButtonLink = document.createElement("a");
+  // updateButtonLink.setAttribute("href", `/post/edit/index.html?id=${postData.id}`);
+  // updateButtonLink.classList.add("col")
+
+  // updatePostContainer.append(updateButtonLink)
 
   
   return post;
@@ -133,7 +132,3 @@ export function postTemplateE(postData) {
 export function renderPostUserTemplate(postData, parent) {
   parent.append(postTemplateE(postData))
 }
-
-// export function renderPostsUserTemplates(postDataList, parent) {
-//   parent.append(...postDataList.map(postTemplateD))
-// }
--- src/js/templates/userPostsEdit.mjs
+++ src/js/templates/userPostsEdit.mjs
@@ -1,20 +1,14 @@
 import * as templatesUser from "./userPostEdit.mjs";
 import { getPost } from "../api/posts/read.mjs";
 
-// async function testPostsUserTemplate() {
-//   const posts = await getPostsUser();
-//   const container = document.querySelector(".postsUserContainer");
-//   templatesUser.renderPostsUserTemplates(posts, container);
-// }
-
-// testPostsUserTemplate()
-
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
  async function testPostUserTemplate() {
   const post = await getPost(id);
+
+  console.log(post)
   const container = document.querySelector("#postUser");
   templatesUser.renderPostUserTemplate(post, container);
 }
